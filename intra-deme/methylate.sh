946cafc002818c0a91cd160827db12e60f5da49f
vesmanojlovic <manojlovicveselin@gmail.com>
2023-07-11 16:50:52 +0100

    scripts for assigning intra-deme methylation

---
+++ intra-deme/methylate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir $1/methylation
+
+for file in $1/simulations/*
+
+do
+    echo "Processing run $(basename ${file})."
+    ./generate_intra-deme.R $file $1/methylation/$(basename ${file}).csv
+    echo "Finished run $(basename ${file})."
+done
+echo "Finished all runs."
