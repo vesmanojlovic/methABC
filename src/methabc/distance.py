4eab012b7b1f47f556ac5793a7735aa0be63a774
vesmanojlovic <manojlovicveselin@gmail.com>
2024-03-18 21:46:40 +0000

    shuffling of synthetic data columns

--- src/methabc/distance.py
+++ src/methabc/distance.py
@@ -1,11 +1,15 @@
 import numpy as np
+import pandas as pd
+
+from itertools import permutations
+from math import factorial
 from scipy.stats import wasserstein_distance
+from scipy.optimize import linear_sum_assignment
 
 
 def squared_distance(gland1, gland2):
     """
-    Compute the squared difference for each fCpG site in two glands. Sum and
-    normalise.
+    Compute the squared distance between two glands' methylation arrays.
     """
     differences = (np.array(gland1) - np.array(gland2)) ** 2
     distance = np.sum(differences)
@@ -15,11 +19,9 @@ def squared_distance(gland1, gland2):
 
 def compute_deme_matrix(df):
     """
-    Compute the distance matrix for a deme. If there are fewer than 8 demes,
-    reject the df automatically.
+    Compute the deme matrix for a given dataframe.
     """
     if 'Side' in df.columns:
-        df = df.sort_values(by=['Side', 'OriginTime']).reset_index(drop=True)
         if df.shape[0] != 8:
             return 100 * np.ones((8, 8))
         else:
@@ -44,8 +46,7 @@ def compute_deme_matrix(df):
 
 def l2_distance(dict1, dict2):
     """
-    Compute the distance between two tumours - L_2 norm of the difference of
-    their distance matrices (multiplied by 1/2 to only include above diagonal).
+    Compute the L_2 distance between two distance matrices.
     """
     df1 = dict1['data']
     df2 = dict2['data']
@@ -58,7 +59,7 @@ def l2_distance(dict1, dict2):
 
 def overall_wasserstein(dict1, dict2):
     """
-    Compute the Wasserstein distance between two tumours.
+    Compute the Wasserstein distance between simulated tumour and data.
     """
     df1 = dict1['data']
     df2 = dict2['data']
@@ -70,4 +71,60 @@ def overall_wasserstein(dict1, dict2):
             res += wasserstein_distance(df1.iloc[i], df2.iloc[i].AverageArray)
         else:
             res += wasserstein_distance(df1.iloc[i], df2.iloc[i])
+    return res
+
+
+def distance_sum(dict1, dict2):
+    return l2_distance(dict1, dict2) + overall_wasserstein(dict1, dict2)
+
+
+def ith_df_perm(df, i):
+    """
+    Return the ith permutation of the dataframe.
+    """
+    left_demes = factorial(len(df[df['Side'] == 'left']))
+    right_demes = factorial(len(df[df['Side'] == 'right']))
+    total_perms = left_demes * right_demes
+
+    if i < 0 or i >= total_perms:
+        raise ValueError('Invalid permutation index.')
+
+    left_df = df[df['Side'] == 'left']
+    right_df = df[df['Side'] == 'right']
+
+    left_perm_index = i // right_demes
+    right_perm_index = i % right_demes
+
+    left_perms = list(permutations(left_df.index))
+    right_perms = list(permutations(right_df.index))
+
+    left_perm = left_perms[left_perm_index]
+    right_perm = right_perms[right_perm_index]
+
+    perm_df = pd.concat([left_df.loc[list(left_perm)],
+                         right_df.loc[list(right_perm)]])
+
+    return perm_df.reset_index(drop=True)
+
+
+def total_distance(dict1, dict2):
+    """
+    Compute the total distance between two tumours.
+    """
+    res = 1000
+    if 'Side' in dict1['data'].columns:
+        tmp_df = dict1['data'].sort_values(by=['Side', 'OriginTime']).reset_index(drop=True)
+        fd = dict2['data']
+    else:
+        tmp_df = dict2['data'].sort_values(by=['Side', 'OriginTime']).reset_index(drop=True)
+        fd = dict1['data']
+    num_l_demes = len(tmp_df[tmp_df['Side'] == 'left'])
+    num_r_demes = len(tmp_df[tmp_df['Side'] == 'right'])
+    iterations = factorial(num_l_demes) * factorial(num_r_demes)
+    for i in range(iterations):
+        perm_df = ith_df_perm(tmp_df, i)
+        tmp_wass = overall_wasserstein({'data': perm_df}, {'data': fd})
+        tmp_l2 = l2_distance({'data': perm_df}, {'data': fd})
+        tmp_dist = tmp_wass + tmp_l2
+        res = min(res, tmp_dist)
     return res
